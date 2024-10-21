a520d409769f9806058249b0c1385afe49c66bcf
agusEmaUade <agyanez@uade.edu.ar>
2024-10-21 20:26:46 -0300

    feat: add template project

---
+++ pal/src/test/java/com/gym/pal/PalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.pal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
